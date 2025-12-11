9d1111513e3ea582fe1bc99548eda9c4019c40f9
Duong Tran <trand000@aol.com>
2025-12-11 07:56:03 +0000

    chnaged labele

--- R/square.R
+++ R/square.R
@@ -2,7 +2,7 @@
 #'
 #' @description A short description of what the function does OK.
 #'
-#' @param x Description of the first argument.
+#' @param x Description of the first argument XX.
 #'
 #' @return Description of the return value.
 #' @examples
@@ -12,3 +12,4 @@
 square <- function(x) {
   x^2
 }
+
